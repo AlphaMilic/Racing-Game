a2dc62791480f9ab7ac2c65df802862e9f802fe3
AlphaMilic <oliver.mili_@htl-wolfsberg.at>
2023-11-29 20:05:14 +0100

    NR4

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    public float accelaration = 1;
+    public float rotationspeed = 1;
+    Rigidbody rigid;
     // Start is called before the first frame update
     void Start()
     {
+        rigid = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0.01f, 0, 0);
+        //if(Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += transform.forward * 0.01f;
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += transform.forward * -0.01f;
+        //}
+
+        float inputVertical = Input.GetAxis("Vertical");
+        float inputHorizontal = Input.GetAxis("Horizontal");
+
+        rigid.AddForce(transform.forward * inputVertical * Time.deltaTime * accelaration);
+        transform.Rotate(new Vector3(0, inputHorizontal * Time.deltaTime * rotationspeed, 0));
+
+
     }
 }
